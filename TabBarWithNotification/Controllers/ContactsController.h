8de670cc7d0eaff7c3ac8337bc1673e59ffb84cb
Brunno dos Santos <squiter85@gmail.com>
2015-10-26 17:09:20 -0200

    Add tabBar and all viewControllers

---
+++ TabBarWithNotification/Controllers/ContactsController.h
@@ -0,0 +1,13 @@
+//
+//  ContactsController.h
+//  TabBarWithNotification
+//
+//  Created by Treinamento Mobile on 10/26/15.
+//  Copyright © 2015 Treinamento Mobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ContactsController : UIViewController
+
+@end
---
+++ TabBarWithNotification/Controllers/ToRatedController.h
@@ -0,0 +1,13 @@
+//
+//  ToRatedController.h
+//  TabBarWithNotification
+//
+//  Created by Treinamento Mobile on 10/26/15.
+//  Copyright © 2015 Treinamento Mobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ToRatedController : UIViewController
+
+@end
