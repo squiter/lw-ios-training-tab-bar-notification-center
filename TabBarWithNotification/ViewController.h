a9d08739e9528e174670de5c0be213d61be88fec
Brunno dos Santos <squiter85@gmail.com>
2015-10-26 17:00:56 -0200

    Initial Commit

---
+++ TabBarWithNotification/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TabBarWithNotification
+//
+//  Created by Treinamento Mobile on 10/26/15.
+//  Copyright © 2015 Treinamento Mobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TabBarWithNotification/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TabBarWithNotification
+//
+//  Created by Treinamento Mobile on 10/26/15.
+//  Copyright © 2015 Treinamento Mobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
